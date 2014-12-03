0c0650fde148abe9a87e60842e200f6b66518cb9
zhuwenxuan <zhuwen_xuan@126.com>
2014-12-03 11:13:37 +0800

    update

--- routes/doc.js
+++ routes/doc.js
@@ -8,11 +8,12 @@ router.get('/:key', function(req, res) {
 	var filename = req.params.key;
 	var dir = path.join(__dirname,"../public/json/");
     var str = File.readFileSync(dir+filename+".js");
-    res.format({
+	res.render('doc',{code:str});    
+/*res.format({
         'application/json':function(){
             res.send(str)
         }
-    })
+    })*/
 });
 
 module.exports = router;
