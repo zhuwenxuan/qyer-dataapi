3038a0ea6ef26ad5d15e65c93e827ef78d30a757
bluers <bluers.z@gmail.com>
2014-12-03 15:48:47 +0800

    update

--- public/json/checkOrder.js
+++ public/json/checkOrder.js
@@ -3,6 +3,7 @@ module.exports.getData = function(){
     return {
         "error_code":0,
         "result":"通过",
+        "error_msg":"error",
         "data":{
             "msg":"消息"     //1.订单号不存在, 2.订单号重复, 3.订单号不符合要求...
         }
